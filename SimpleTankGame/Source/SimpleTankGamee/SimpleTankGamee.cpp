74be40bcc5dea10775b1ffd95a48bd2cf9e0bb31
Saeed Jannati <msaeedjannati72@gmail.com>
2023-08-02 19:20:06 +0330

    Creating unreal project

---
+++ SimpleTankGame/Source/SimpleTankGamee/SimpleTankGamee.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleTankGamee.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleTankGamee, "SimpleTankGamee" );
---
+++ SimpleTankGame/Source/SimpleTankGamee/SimpleTankGameeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleTankGameeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLETANKGAMEE_API ASimpleTankGameeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
